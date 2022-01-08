2f80729790569182e314d10bf6d47a3d9f7b2edb
Bilal BOUALEM <bilal.boualem1@gmail.com>
2022-01-08 11:36:48 +0000

    login to db

--- vitrine/src/app/app.component.ts
+++ vitrine/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Realm from "realm-web";
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.logIn();
+  }
+
   title = 'Formation JS';
+  patients:{"nom":string, "prenom":string}[] = [];
+
+  async logIn(){
+    const app = new Realm.App({id:environment.projectID});
+    const credentials = Realm.Credentials.anonymous();
+    try {
+      const user: Realm.User = await app.logIn(credentials);
+      this.patients = await user.functions['getAllPatients']();
+      console.log(this.patients);
+      if(user.id === app.currentUser?.id)
+      return user;
+      else 
+      return undefined;
+    } catch(err) {
+      console.error("Failed to log in", err);
+      return undefined;
+    } 
+  }
 }
