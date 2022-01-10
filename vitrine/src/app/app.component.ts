b95c32f18f4dba0115d25b1ee83068c837eb4a0a
Bilal BOUALEM <bilal.boualem1@gmail.com>
2022-01-10 19:31:37 +0000

    clean

--- vitrine/src/app/app.component.ts
+++ vitrine/src/app/app.component.ts
@@ -7,18 +7,20 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  ngOnInit(): void {
-    this.logIn();
-  }
+  
 
   title = 'Formation JS';
   patients:{"nom":string, "prenom":string}[] = [];
 
+  ngOnInit(): void {
+      this.logIn();
+    }
   async logIn(){
     const app = new Realm.App({id:environment.projectID});
     const credentials = Realm.Credentials.anonymous();
     try {
       const user: Realm.User = await app.logIn(credentials);
+      console.log(user);
       this.patients = await user.functions['getAllPatients']();
       console.log(this.patients);
       if(user.id === app.currentUser?.id)
